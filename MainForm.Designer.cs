367dcbbaf005fcea4c73d842caa8b02e8b360099
juan21 <juan.corona@alumno.udg.mx>
2013-11-05 22:14:26 -0600

    practica6 encuesta

---
+++ MainForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace practica6
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		private void InitializeComponent()
+		{
+			this.id1 = new System.Windows.Forms.RadioButton();
+			this.id2 = new System.Windows.Forms.RadioButton();
+			this.id3 = new System.Windows.Forms.RadioButton();
+			this.codigo = new System.Windows.Forms.Label();
+			this.nombre = new System.Windows.Forms.Label();
+			this.cod = new System.Windows.Forms.TextBox();
+			this.nom = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.portugal = new System.Windows.Forms.CheckBox();
+			this.chile = new System.Windows.Forms.CheckBox();
+			this.ru = new System.Windows.Forms.CheckBox();
+			this.mac = new System.Windows.Forms.CheckBox();
+			this.australia = new System.Windows.Forms.CheckBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.Calendar1 = new System.Windows.Forms.MonthCalendar();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			 
+			// id1
+			 
+			this.id1.Location = new System.Drawing.Point(12, 103);
+			this.id1.Name = "id1";
+			this.id1.Size = new System.Drawing.Size(104, 24);
+			this.id1.TabIndex = 0;
+			this.id1.TabStop = true;
+			this.id1.Text = "DF";
+			this.id1.UseVisualStyleBackColor = true;
+			
+			// id2
+			 
+			this.id2.Location = new System.Drawing.Point(12, 133);
+			this.id2.Name = "id2";
+			this.id2.Size = new System.Drawing.Size(104, 24);
+			this.id2.TabIndex = 1;
+			this.id2.TabStop = true;
+			this.id2.Text = "Guadalajara";
+			this.id2.UseVisualStyleBackColor = true;
+			 
+			// id3
+			 
+			this.id3.Location = new System.Drawing.Point(12, 172);
+			this.id3.Name = "id3";
+			this.id3.Size = new System.Drawing.Size(104, 24);
+			this.id3.TabIndex = 2;
+			this.id3.TabStop = true;
+			this.id3.Text = "Monterrey";
+			this.id3.UseVisualStyleBackColor = true;
+			 
+			// codigo
+			 
+			this.codigo.Location = new System.Drawing.Point(12, 9);
+			this.codigo.Name = "codigo";
+			this.codigo.Size = new System.Drawing.Size(100, 23);
+			this.codigo.TabIndex = 3;
+			this.codigo.Text = "Codigo";
+			 
+			// nombre
+			 
+			this.nombre.Location = new System.Drawing.Point(12, 43);
+			this.nombre.Name = "nombre";
+			this.nombre.Size = new System.Drawing.Size(100, 23);
+			this.nombre.TabIndex = 4;
+			this.nombre.Text = "Nombre";
+			
+			// cod
+			 
+			this.cod.Location = new System.Drawing.Point(57, 9);
+			this.cod.Name = "cod";
+			this.cod.Size = new System.Drawing.Size(100, 20);
+			this.cod.TabIndex = 5;
+			 
+			// nom
+			 
+			this.nom.Location = new System.Drawing.Point(57, 43);
+			this.nom.Name = "nom";
+			this.nom.Size = new System.Drawing.Size(100, 20);
+			this.nom.TabIndex = 6;
+			 
+			// label3
+			 
+			this.label3.Location = new System.Drawing.Point(12, 77);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(117, 23);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "1.- Capital de Mexico";
+			 
+			// label4
+			
+			this.label4.Location = new System.Drawing.Point(12, 209);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(117, 23);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "2.-Paises de Europa";
+			 
+			// portugal
+			 
+			this.portugal.Location = new System.Drawing.Point(12, 235);
+			this.portugal.Name = "portugal";
+			this.portugal.Size = new System.Drawing.Size(104, 24);
+			this.portugal.TabIndex = 9;
+			this.portugal.Text = "Portugal";
+			this.portugal.UseVisualStyleBackColor = true;
+			 
+			// chile
+			 
+			this.chile.Location = new System.Drawing.Point(12, 265);
+			this.chile.Name = "chile";
+			this.chile.Size = new System.Drawing.Size(104, 24);
+			this.chile.TabIndex = 10;
+			this.chile.Text = "Chile";
+			this.chile.UseVisualStyleBackColor = true;
+			 
+			// ru
+			 
+			this.ru.Location = new System.Drawing.Point(12, 295);
+			this.ru.Name = "ru";
+			this.ru.Size = new System.Drawing.Size(104, 24);
+			this.ru.TabIndex = 11;
+			this.ru.Text = "Reino Unido";
+			this.ru.UseVisualStyleBackColor = true;
+			 
+			// mac
+			 
+			this.mac.Location = new System.Drawing.Point(106, 235);
+			this.mac.Name = "mac";
+			this.mac.Size = new System.Drawing.Size(104, 24);
+			this.mac.TabIndex = 12;
+			this.mac.Text = "Macedonia";
+			this.mac.UseVisualStyleBackColor = true;
+			// 
+			// australia
+			// 
+			this.australia.Location = new System.Drawing.Point(106, 265);
+			this.australia.Name = "australia";
+			this.australia.Size = new System.Drawing.Size(104, 24);
+			this.australia.TabIndex = 13;
+			this.australia.Text = "Australia";
+			this.australia.UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(12, 322);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(287, 23);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "3.-Cantidad de Estados de los que se compone Mexico";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(25, 348);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
+			this.numericUpDown1.TabIndex = 15;
+			// 
+			// Calendar1
+			// 
+			this.Calendar1.Location = new System.Drawing.Point(251, 70);
+			this.Calendar1.Name = "Calendar1";
+			this.Calendar1.TabIndex = 16;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(251, 266);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(107, 23);
+			this.button1.TabIndex = 17;
+			this.button1.Text = "Terminar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(240, 43);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(238, 23);
+			this.label6.TabIndex = 18;
+			this.label6.Text = "4.-Dia en que termino la segunda guerra mundial";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(389, 266);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 19;
+			this.button2.Text = "Salir";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(556, 393);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.Calendar1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.australia);
+			this.Controls.Add(this.mac);
+			this.Controls.Add(this.ru);
+			this.Controls.Add(this.chile);
+			this.Controls.Add(this.portugal);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.nom);
+			this.Controls.Add(this.cod);
+			this.Controls.Add(this.nombre);
+			this.Controls.Add(this.codigo);
+			this.Controls.Add(this.id3);
+			this.Controls.Add(this.id2);
+			this.Controls.Add(this.id1);
+			this.Name = "MainForm";
+			this.Text = "practica6";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.MonthCalendar Calendar1;
+		private System.Windows.Forms.Label codigo;
+		private System.Windows.Forms.Label nombre;
+		private System.Windows.Forms.RadioButton id1;
+		private System.Windows.Forms.RadioButton id2;
+		private System.Windows.Forms.RadioButton id3;
+		private System.Windows.Forms.TextBox cod;
+		private System.Windows.Forms.TextBox nom;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.CheckBox australia;
+		private System.Windows.Forms.CheckBox mac;
+		private System.Windows.Forms.CheckBox ru;
+		private System.Windows.Forms.CheckBox chile;
+		private System.Windows.Forms.CheckBox portugal;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace practica6
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			String codigo = cod.Text;
+			String nombre = nom.Text;
+			
+			int aciertos=0;
+			if(this.id1.Checked==true){
+				aciertos++;
+			}
+			if (this.portugal.Checked==true){
+				aciertos++;
+			}
+			if(this.ru.Checked==true){
+				aciertos++;
+			}
+			if (this.mac.Checked==true){
+				aciertos++;
+			}
+			if(this.numericUpDown1.Text == "32"){
+                aciertos++;
+			}
+
+			DateTime fecha = Calendar1.SelectionStart;
+
+			if(fecha.ToShortDateString()=="07/05/1945"){
+				aciertos++;
+			}
+
+			MessageBox.Show("codigo: "+cod.Text+"\nnombre: "+nom.Text+"\naciertos: "+aciertos);	
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
